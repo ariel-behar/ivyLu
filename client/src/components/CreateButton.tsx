342d4ea57edee901ea0d7c53c00ab813d4b7da52
Ariel Behar <ariel.behar@hotmail.com>
2022-12-28 16:00:47 +0200

    c: Creating a CreateButton component

---
+++ client/src/components/CreateButton.tsx
@@ -0,0 +1,32 @@
+import Button from '@mui/material/Button';
+import { Link as RouterLink, } from 'react-router-dom';
+
+import AddIcon from '@mui/icons-material/Add';
+
+type CreateButtonProps = {
+	item: 'Service' | 'Product'
+}
+
+function CreateButton({
+	item
+}: CreateButtonProps) {
+
+	const getEntity = () => {
+		return item.toLowerCase().slice(0, item.length).concat('s')
+	}
+
+	return (
+		<Button
+			to={`/management/${getEntity()}/create`}
+			variant='text'
+			size='small'
+			sx={{ marginTop: '20px', marginBottom: '20px' }}
+			component={RouterLink}
+			endIcon={<AddIcon />}
+		>
+			Create {item}
+		</Button>
+	)
+}
+
+export default CreateButton
--- client/src/views/AuthorizedViews/ProductViews/ProductsManagementView.tsx
+++ client/src/views/AuthorizedViews/ProductViews/ProductsManagementView.tsx
@@ -1,24 +1,14 @@
-import Button from '@mui/material/Button';
-import { Outlet, Link as RouterLink, } from 'react-router-dom';
+import { Outlet, } from 'react-router-dom';
 
-import AddIcon from '@mui/icons-material/Add';
 import { Stack } from '@mui/system';
+import CreateButton from '../../../components/CreateButton';
 
 function ProductsManagementView() {
 	return (
 		<>
 			<div>ProductsManagementView</div>
 			<Stack direction='row' justifyContent='end'>
-				<Button
-					to='/management/services/create'
-					variant='text'
-					size='small'
-					sx={{ marginTop: '20px', marginBottom: '20px' }}
-					component={RouterLink}
-					endIcon={<AddIcon />}
-				>
-					Create Product
-				</Button>
+				<CreateButton item={'Product'} />
 			</Stack>
 
 			<Outlet />
