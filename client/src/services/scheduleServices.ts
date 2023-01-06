8ac3bb086f42d17376533344ac69d197a815788b
Ariel Behar <ariel.behar@hotmail.com>
2023-01-06 11:37:50 +0200

    c: created the ServiceDetailsCard, schema and respective services

---
+++ client/src/services/scheduleServices.ts
@@ -0,0 +1,47 @@
+import { AuthTokenType, Identifiable } from "../types/common/commonTypes";
+import request from "../utils/request";
+
+let baseUrl = 'http://localhost:3030'
+
+export interface ApiSchedule<I, E extends Identifiable<I>, A extends AuthTokenType> {
+
+    getHairdresserSchedule(hairdresserId: I): Promise<E | E[]>
+
+    // getOne(entityId: I): Promise<E>;
+    // getAll(): Promise<E[]>;
+    create(appointmentWithoutId: Omit<E, 'id'>, authToken: A): Promise<E>;
+    // update(entityId: I, entity: E, authToken: A): Promise<E>;
+    // deleteOne(entityId: I, entity: undefined, authToken: A): Promise<string>;
+}
+
+export class ApiScheduleImpl<I, E extends Identifiable<I>, A extends AuthTokenType> implements ApiSchedule<I,E,A> {
+    constructor(public apiCollectionSuffix: string) {}
+
+
+    getHairdresserSchedule(hairdresserId: I): Promise<E | E[]> {
+        return request(`${baseUrl}/${this.apiCollectionSuffix}/${hairdresserId}`, 'GET')
+    }
+
+    create(appointmentWithoutId: Omit<E, "id">, authToken: A): Promise<E> {
+        return request(`${baseUrl}/${this.apiCollectionSuffix}/create`, 'POST', appointmentWithoutId, authToken);
+    }
+
+    // getOne(entityId: I): Promise<E> {
+    //     return request(`${baseUrl}/${this.apiCollectionSuffix}/${entityId}`, 'GET');
+    // }
+    // getAll(): Promise<E[]> {
+    //     return request(`${baseUrl}/${this.apiCollectionSuffix}`, 'GET');
+    // }
+    // create(entityWithoutId: Omit<E, "id">, userId: I, authToken: A): Promise<E> {
+    //     return request(`${baseUrl}/${this.apiCollectionSuffix}/create`, 'POST', {...entityWithoutId, userId}, authToken);
+    // }
+    // update(entityId: I, entity: E, authToken: A): Promise<E> {
+    //     return request(`${baseUrl}/${this.apiCollectionSuffix}/${entityId}/edit`, 'POST', entity, authToken);
+    // }
+    // deleteOne(entityId: I, entity: undefined, authToken: A): Promise<string> {
+    //     return request(`${baseUrl}/${this.apiCollectionSuffix}/${entityId}/delete`, 'GET', entity, authToken);
+    // }
+ 
+
+}
+
--- client/src/views/AuthorizedViews/ServicesViews/ServiceDetailsCard.tsx
+++ client/src/views/AuthorizedViews/ServicesViews/ServiceDetailsCard.tsx
@@ -1,9 +1,16 @@
 import React, { useEffect, useState } from 'react'
+import uniqid from 'uniqid'
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useLoaderData } from 'react-router-dom'
 
 import { Service } from "../../../models/Service"
+import { User } from '../../../models/User'
+import { Schedule, ScheduleCreateDTO } from '../../../models/Schedule';
+import { ApiSchedule, ApiScheduleImpl } from '../../../services/scheduleServices';
+import { AuthTokenType, IdType } from '../../../types/common/commonTypes';
+import addScheduleFormSchema from '../../../validations/addScheduleFormSchema';
+import { availableSchedulingHours, AvailableSchedulingHoursType } from '../../../utils/availableSchedulingHours';
 
 import Grid from "@mui/material/Grid"
 import ImageListItem from "@mui/material/ImageListItem"
@@ -12,6 +19,7 @@ import Typography from "@mui/material/Typography"
 import Button from "@mui/material/Button"
 import TextField from '@mui/material/TextField'
 import Paper from '@mui/material/Paper'
+import MenuItem from '@mui/material/MenuItem'
 
 import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
 import format from 'date-fns/format'
@@ -21,38 +29,58 @@ import getDay from 'date-fns/getDay';
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
-import { User } from '../../../models/User'
-import MenuItem from '@mui/material/MenuItem'
-import uniqid from 'uniqid'
+import { useAuthContext } from '../../../contexts/AuthContext';
+import { useNotificationContext } from '../../../contexts/NotificationContext';
 
-let availabelHours = ['10:00', '10:30', '11:00', '11:30', '12:00', '12:30', '13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00', '17:30', '18:00', '18:30', '19:00', '19:30', '20:00', '20:30', '21:00', '21:30'] as const
+
+type FormData = {
+	hairdresser: string,
+	appointmentDate: Date,
+	appointmentHour: AvailableSchedulingHoursType,
+}
+
+const scheduleServices: ApiSchedule<IdType, Schedule, AuthTokenType> = new ApiScheduleImpl<IdType, Schedule, AuthTokenType>('schedule');
 
 interface ServiceDetailsCardProps {
 	service: Service
 }
 
-const locales = {
-	"en-US": require('date-fns/locale/en-US')
-}
-
 function ServiceDetailsCard({ service }: ServiceDetailsCardProps) {
 	const { hairdressers } = useLoaderData() as { hairdressers: User[] };
-	const [selectedHairdresser, setSelectedHairdresser] = useState<User>(hairdressers[0])
-	const [selectedCalendarDate, setSelectedCalendarDate] = useState<Date>(new Date());
-	const [selectedHour, setSelectedHour] = useState<typeof availabelHours>()
+	const { user, isClient } = useAuthContext() as any;
+	const { displayNotification } = useNotificationContext() as any;
+	const [selectedHairdresser, setSelectedHairdresser] = useState<User | ''>('')
+	const [hairdresserSchedule, setHairdresserSchedule] = useState<Schedule | null>(null);
+	const [selectedAppointmentDate, setSelectedAppointmentDate] = useState<Date>(new Date());
+	const [selectedAppointmentHour, setSelectedAppointmentHour] = useState<AvailableSchedulingHoursType>(availableSchedulingHours[0])
 	const [isCalendarOpen, setIsCalendarOpen] = useState(false);
 
+	const { register, handleSubmit, formState: { errors, isDirty, isValid } } = useForm<FormData>({
+		mode: 'onBlur',
+		resolver: yupResolver(addScheduleFormSchema),
+		defaultValues: {
+			hairdresser: '',
+			appointmentDate: new Date(),
+			appointmentHour: availableSchedulingHours[0]
+		}
+	})
 
 	useEffect(() => {
-		// Implement a request to get the schedule for the selected hairdresser
-		console.log('useffect')
+		if (selectedHairdresser !== '') {
+			scheduleServices.getHairdresserSchedule(selectedHairdresser._id)
+				.then((data) => {
+					setHairdresserSchedule(data as Schedule);
+				})
+				.catch(err => console.log(err))
+		}
 		return () => {
 		}
-	}, [selectedCalendarDate])
+	}, [selectedHairdresser])
 
 	const handleDateChange = (e: Date) => {
+		// Need to implement that whenever the date is changed, the hours also need to be updated according to availablity. If chosen "today", it shouldn't display the hours that have already passed today
 		setIsCalendarOpen(!isCalendarOpen);
-		setSelectedCalendarDate(e);
+		setSelectedAppointmentDate(e);
 	};
 
 	const toggleCalendarIsOpen = () => setIsCalendarOpen(!isCalendarOpen);
@@ -63,15 +91,31 @@ function ServiceDetailsCard({ service }: ServiceDetailsCardProps) {
 	}
 
 	const handleHourChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		console.log(e.target.value)
-		// setSelectedHour(e.target.value)
+		setSelectedAppointmentHour(e.target.value as AvailableSchedulingHoursType)
 	}
 
-	// function onSubmit(e: React.MouseEvent<HTMLButtonElement>) {
-	// 	e.preventDefault()
+	const onFormSubmit = async (data: FormData, e: React.BaseSyntheticEvent<object, any, any> | undefined) => {
+		e?.preventDefault()
 
-	// 	console.log('here')
-	// }
+		const appointment: ScheduleCreateDTO = {
+			clientId: user.userId,
+			hairdresserId: data.hairdresser,
+			serviceId: service._id,
+			date: selectedAppointmentDate,
+			hour: data.appointmentHour
+		}
+
+		try {
+			let createScheduleItemResponse = await scheduleServices.create(appointment as Schedule, user.authToken)
+
+			console.log('createScheduleItemResponse:', createScheduleItemResponse)
+
+		} catch (err) {
+			displayNotification(err, 'error')
+		}
+
+
+	}
 
 	return (
 		<>
@@ -99,71 +143,100 @@ function ServiceDetailsCard({ service }: ServiceDetailsCardProps) {
 							<Typography variant="h6" component='h6'>Duration: <b>{service.duration} hrs</b></Typography>
 						</Stack>
 
+						{isClient &&
+							<form onSubmit={handleSubmit(onFormSubmit)}>
+								<TextField
+									required
+									label='Select Hairdresser'
+									select
+									value={selectedHairdresser !== '' ? selectedHairdresser._id : ''}
+									{...register('hairdresser')}
+									onChange={handleHairdresserChange}
+									variant="outlined"
+									size="small"
+									fullWidth
+									error={errors.hairdresser ? true : false}
+									helperText={errors.hairdresser ? errors.hairdresser.message : ''}
+								>
+									{
+										Object.values(hairdressers).map(hairdresser => (
+											<MenuItem
+												key={uniqid()}
+												value={hairdresser._id}
+											>
+												{`${hairdresser.firstName} ${hairdresser.lastName}`}
+											</MenuItem>
+										))
+									}
+								</TextField>
 
-						<form>
-							<TextField
-								required
-								label='Select Hairdresser'
-								select
-								value={selectedHairdresser._id}
-								// {...register('hairdresser')}
-								onChange={handleHairdresserChange}
-								variant="outlined"
-								size="small"
-								fullWidth
-							// error={errors.volumeMeasurementUnit ? true : false}
-							// helperText={errors.volumeMeasurementUnit ? errors.volumeMeasurementUnit.message : ''}
-							>
+
 								{
-									Object.values(hairdressers).map(hairdresser => (
-										<MenuItem key={uniqid()} value={hairdresser._id}>{`${hairdresser.firstName} ${hairdresser.lastName}`}</MenuItem>
-									))
+									selectedHairdresser &&
+									<>
+										<TextField
+											required
+											variant='outlined'
+											onClick={toggleCalendarIsOpen}
+											label="Select Appointment Date"
+											{...register('appointmentDate')}
+											value={format(selectedAppointmentDate, "d MMMM, yyyy")}
+											size="small"
+											fullWidth
+											error={errors.appointmentDate ? true : false}
+											helperText={errors.appointmentDate ? errors.appointmentDate.message : ''}
+										/>
+
+										{
+											isCalendarOpen &&
+											 <DatePicker
+												selected={selectedAppointmentDate}
+												highlightDates={[new Date()]}
+												minDate={new Date()}
+												showDisabledMonthNavigation
+												onChange={(date: Date) => {
+													console.log('date:', date)
+													setSelectedAppointmentDate(date as Date)
+													toggleCalendarIsOpen()
+												}}
+												inline
+											/>
+										}
+
+										<TextField
+											required
+											label='Select Available Hour'
+											select
+											value={selectedAppointmentHour}
+											{...register('appointmentHour')}
+											onChange={handleHourChange}
+											variant="outlined"
+											size="small"
+											fullWidth
+											sx={{ maxHeight: '100px' }}
+											error={errors.appointmentHour ? true : false}
+											helperText={errors.appointmentHour ? errors.appointmentHour.message : ''}
+										>
+											{
+												availableSchedulingHours.map(hour => (
+													<MenuItem key={uniqid()} value={hour}>{hour}</MenuItem>
+												))
+											}
+										</TextField>
+
+										<Button
+											sx={{ display: 'block' }}
+											variant='contained'
+											style={{ marginTop: '10px' }}
+											type="submit"
+											disabled={isDirty && isValid}
+										>
+											Create Appointment
+										</Button>
+									</>
 								}
-							</TextField>
-
-							<TextField
-								required
-								variant='outlined'
-								onClick={toggleCalendarIsOpen}
-								label="Select Appointment Date"
-								value={format(selectedCalendarDate, "d MMMM, yyyy")}
-								size="small"
-								fullWidth
-							/>
-
-							{isCalendarOpen && <DatePicker
-								selected={selectedCalendarDate}
-								onChange={(date: Date) => {
-									setSelectedCalendarDate(date)
-									toggleCalendarIsOpen()
-								}}
-								inline
-							/>
-							}
-
-							<TextField
-								required
-								label='Select Available Hour'
-								select
-								value={selectedHour}
-								// {...register('hairdresser')}
-								onChange={handleHourChange}
-								variant="outlined"
-								size="small"
-								sx={{maxHeight: '100px'}}
-								fullWidth
-							// error={errors.volumeMeasurementUnit ? true : false}
-							// helperText={errors.volumeMeasurementUnit ? errors.volumeMeasurementUnit.message : ''}
-							>
-								{
-									availabelHours.map(hour => (
-										<MenuItem key={uniqid()} value={hour}>{hour}</MenuItem>
-									))
-								}
-							</TextField>
-
-							<Button variant='contained' style={{ marginTop: '10px' }} type="submit">Add Event</Button>
-						</form>
+							</form>
+						}
 					</Grid>
 				</Grid>
 			</Paper>
