6d57be16d1d891786b78c5eb850ccbd949fa1d12
Ariel Behar <ariel.behar@hotmail.com>
2023-01-06 11:38:39 +0200

    s: created the scheduleController and scheduleService

--- server/src/controllers/scheduleController.js
+++ server/src/controllers/scheduleController.js
@@ -1,14 +1,92 @@
-import {Router} from 'express'
+import { Router } from 'express'
+import * as scheduleServices from '../services/scheduleServices.js'
+import { isAuth, isClient } from '../middlewares/authMiddleware.js'
+import format from 'date-fns/format/index.js'
 
 const router = Router();
 
-router.post('/add', (req, res) => {
-    console.log(req.body)
+router.get('/:hairdresserId', async (req, res) => {
+    const hairdresserId = req.params.hairdresserId;
 
-    try{
+    try {
+        let hairdresserScheduleResponse = await scheduleServices.getHairdresserSchedule(hairdresserId)
 
-    } catch(err) {
-        console.log(err);
+        if (hairdresserScheduleResponse) {
+            res.json(hairdresserScheduleResponse)
+        }
+
+    } catch (err) {
+        res.status(500).send(err)
+    }
+
+})
+
+router.post('/create', isAuth, isClient, async (req, res) => {
+    const { clientId, hairdresserId, serviceId, date, hour } = req.body;
+
+    try {
+        let formattedDateISO = format(new Date(date), "dd/MM/yyyy")
+        console.log('formattedDateIso:', typeof formattedDateISO)
+
+        let appointmentExists = await scheduleServices.findOneByDateAndHour(formattedDateISO, hour)
+
+        if (appointmentExists) {
+            throw {statusCode: 403, message: "An appointment on this Date and Hour already exists. Please pick another time"}
+        } else {
+            try {
+                let createScheduleItemResponse = await scheduleServices.create({ clientId, hairdresserId, serviceId, date, hour })
+
+                if (createScheduleItemResponse) {
+                    let scheduledItemId = createScheduleItemResponse._id;
+
+                    try {
+                        let scheduledItemResponse = await scheduleServices.getOneAndPopulate(scheduledItemId)
+
+                        let scheduledItem = {
+                            _id: scheduledItemResponse._id,
+                            client: {
+                                firstName: scheduledItemResponse.clientId.firstName,
+                                lastName: scheduledItemResponse.clientId.lastName,
+                                phone: scheduledItemResponse.clientId.phone,
+                                gender: scheduledItemResponse.clientId.gender,
+                                email: scheduledItemResponse.clientId.email,
+                            },
+                            hairdresser: {
+                                firstName: scheduledItemResponse.hairdresserId.firstName,
+                                lastName: scheduledItemResponse.hairdresserId.lastName,
+                                phone: scheduledItemResponse.hairdresserId.phone,
+                                gender: scheduledItemResponse.hairdresserId.gender,
+                                imgUrl: scheduledItemResponse.hairdresserId.imgUrl
+                            },
+                            service: {
+                                title: scheduledItemResponse.serviceId.firstName,
+                                description: scheduledItemResponse.serviceId.lastName,
+                                imgUrl: scheduledItemResponse.serviceId.imgUrl,
+                                price: scheduledItemResponse.serviceId.price,
+                                duration: scheduledItemResponse.serviceId.duration,
+                            },
+                            appointmentDetails: {
+                                hour: scheduledItemResponse.hour,
+                                date: scheduledItemResponse.date,
+                                dateISO: scheduledItemResponse.dateISO,
+                                day: scheduledItemResponse.day,
+                                dayOfWeek: scheduledItemResponse.dayOfWeek,
+                                month: scheduledItemResponse.month,
+                                monthName: scheduledItemResponse.monthName,
+                                year: scheduledItemResponse.year
+                            }
+                        }
+                        res.json(scheduledItem)
+                    } catch (err) {
+                        res.status(err.statusCode ? err.statusCode : 500).json(err)
+                    }
+                }
+            } catch (err) {
+                res.status(err.statusCode ? err.statusCode : 500).json(err)
+            }
+        }
+    } catch (err) {
+        res.status(err.statusCode ? err.statusCode : 500).json(err)
     }
 })
 
--- server/src/services/scheduleServices.js
+++ server/src/services/scheduleServices.js
@@ -1,4 +1,12 @@
-import Schedule from '../models/Schedule'
+import Schedule from '../models/Schedule.js'
 
-export const add = (clientId, hairdresserId, serviceId, date) => Schedule.create({clientId, hairdresserId, serviceId, date})
+export const create = ({clientId, hairdresserId, serviceId, date, hour}) => Schedule.create({clientId, hairdresserId, serviceId, date, hour})
+
+export const getHairdresserSchedule = (hairdresserId) => Schedule.find({hairdresserId}).lean();
+
+export const getOne = (scheduledItemId) => Schedule.find({_id: scheduledItemId})
+
+export const getOneAndPopulate = (scheduledItemId) => Schedule.findById(scheduledItemId).populate('hairdresserId').populate('clientId').populate('serviceId')
+
+export const findOneByDateAndHour = (dateISO, hour) => Schedule.findOne({dateISO: dateISO, hour: hour})
 
