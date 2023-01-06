e29d9c7e14d3ce5dd38852046caa7577ebc0de3c
Ariel Behar <ariel.behar@hotmail.com>
2023-01-06 14:43:22 +0200

    c: updating ServiceGuestCustomerView link to lead to login if user is not logged

--- client/src/views/ServiceViews/ServicesGuestCustomerView.tsx
+++ client/src/views/ServiceViews/ServicesGuestCustomerView.tsx
@@ -8,10 +8,12 @@ import ImageListItem from "@mui/material/ImageListItem";
 import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
+import { useAuthContext } from "../../contexts/AuthContext";
 
 
 function ServicesGuestCustomerView() {
     const services = useLoaderData() as Service[];
+    const { isLoggedIn } = useAuthContext() as any;
 
     return (
         <>
@@ -24,51 +26,48 @@ function ServicesGuestCustomerView() {
             >
                 {
                     services.map(service => (
-                        <RouterLink to={`/services/${service._id}/details`} key={uniqid()}>
-                            <ImageListItem >
-                                <img style={{ width: 'auto', height: '400px' }}
-                                    src={`${service.imgUrl}`}
-                                    alt={service.title}
-                                    loading='lazy'
-                                />
-                                <Stack
-                                    direction='row'
-                                    justifyContent='center'
-                                    py={2}
-                                    px={1}
-                                    width='100%'
-                                    position='absolute'
-                                    top='0'
-                                    sx={{
-                                        backgroundColor: 'rgba(30, 30, 30, 0.5)'
-                                    }}
-                                >
-                                    <Typography variant="h6" color='white' component='h6'>{service.title}</Typography>
+                        <ImageListItem key={uniqid()}>
+                            <img style={{ width: 'auto', height: '400px' }}
+                                src={`${service.imgUrl}`}
+                                alt={service.title}
+                                loading='lazy'
+                            />
+                            <Stack
+                                direction='row'
+                                justifyContent='center'
+                                py={2}
+                                px={1}
+                                width='100%'
+                                position='absolute'
+                                top='0'
+                                sx={{
+                                    backgroundColor: 'rgba(30, 30, 30, 0.5)'
+                                }}
+                            >
+                                <Typography variant="h6" color='white' component='h6'>{service.title}</Typography>
+                            </Stack>
+
+                            <Stack
+                                direction='row'
+                                justifyContent='space-around'
+                                spacing={1}
+                                py={2}
+                                width='100%'
+                                position='absolute'
+                                bottom='0'
+                                sx={{
+                                    backgroundColor: 'rgba(30, 30, 30, 0.5)'
+                                }}
+                            >
+                                <Button variant="contained" to={isLoggedIn ? `/services/${service._id}/schedule` : `/login`} component={RouterLink}>Schedule</Button>
+
+                                <Stack direction='column'>
+                                    <Typography variant="body1" sx={{ color: 'common.white' }}>Duration: <b> {service.duration} hrs</b></Typography>
+                                    <Typography variant="body1" sx={{ color: 'common.white' }}>Price: <b>{service.price} BGN</b></Typography>
+
                                 </Stack>
-
-                                <Stack
-                                    direction='row'
-                                    justifyContent='space-around'
-                                    spacing={1}
-                                    py={2}
-                                    width='100%'
-                                    position='absolute'
-                                    bottom='0'
-                                    sx={{
-                                        backgroundColor: 'rgba(30, 30, 30, 0.5)'
-                                    }}
-                                >
-                                    <Button variant="contained" to={`/services/${service._id}/schedule`} component={RouterLink}>Schedule</Button>
-
-                                    <Stack direction='column'>
-                                        <Typography variant="body1" sx={{ color: 'common.white' }}>Duration: <b> {service.duration} hrs</b></Typography>
-                                        <Typography variant="body1" sx={{ color: 'common.white' }}>Price: <b>{service.price} BGN</b></Typography>
-
-                                    </Stack>
-                                </Stack>
-                            </ImageListItem>
-
-                        </RouterLink>
+                            </Stack>
+                        </ImageListItem>
                     ))
                 }
             </ImageList>
