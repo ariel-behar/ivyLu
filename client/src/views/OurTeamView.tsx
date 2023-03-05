5fbee8b14a47dfa436a3918b538c345ca671ed49
Ariel Behar <ariel.behar@hotmail.com>
2023-03-05 11:29:31 +0200

    Optimizing OurTeamView for mobile

--- client/src/views/OurTeamView.tsx
+++ client/src/views/OurTeamView.tsx
@@ -17,40 +17,38 @@ function OurTeamView() {
 
 	return (
 		<Box py={3}>
-			<Container sx={{ height: '80vh' }}>
+			<Container sx={{ minHeight: '80vh' }}>
 				<div>OurTeamView</div>
 
 				<Stack direction='row' alignItems='center' sx={{overflow: 'hidden'}}>
 					<Typography variant="h3" sx={{ color: 'common.white', marginRight: '30px', whiteSpace: 'nowrap' }}>Meet Our Team!</Typography>
 					<hr style={{ minWidth: '100%', height: '2px' }} />
 				</Stack>
+
 				<Stack direction='row' height='100%'>
 
 					<ImageList
-						sx={{ width: '100%' }}
-						cols={3}
-						rowHeight={400}
+						sx={{ 
+							width: '100%', 
+							gridTemplateColumns: {xs: 'repeat(1, 1fr)!important', sm: 'repeat(2, 1fr)!important', md: 'repeat(3, 1fr)!important'}
+						}}
+						gap={10}
 					>
 						{
 							hairdressers.map((hairdresser: User) => {
 								return (
 									<ImageListItem key={uniqid()}>
-										<img
-											src={hairdresser.imgUrl}
-											alt={hairdresser.firstName}
-											loading='lazy'
-										/>
+										<img src={hairdresser.imgUrl} alt={hairdresser.firstName} loading='lazy' />
+
 										<ImageListItemBar position='top' title={`${hairdresser.firstName} ${hairdresser.lastName}`} />
+
 										<ImageListItemBar subtitle={getUserRole(hairdresser.role).capitalized} />
 
 									</ImageListItem>
 								)
 							})
 						}
-
 					</ImageList >
-
-
 				</Stack >
 			</Container >
 		</Box>
