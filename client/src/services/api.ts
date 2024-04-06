2b58d17117cb3dbea85952c850414aab13868539
Ariel Behar <ariel.behar@hotmail.com>
2024-04-06 09:19:02 +0300

    Deleting all files

--- client/src/services/api.ts
+++
@@ -1,4 +0,0 @@
-// export const baseUrl = 'http://localhost:3030/api'
-// export const baseUrl = 'https://ivylu.herokuapp.com/api'
-// export const baseUrl = 'http://ivylu.arielbehar.com/api'
-export const baseUrl = 'https://ivylu.onrender.com/api'
--- server/src/config/initDatabase.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose'
-import * as env from 'dotenv'
-env.config()
-
-export default function initDatabase (dbName: string){
-    mongoose.set("strictQuery", false);
-    return mongoose.connect(`${process.env.MONGODB_ATLAS_URI}/${dbName}`);
-}
