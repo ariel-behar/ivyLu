a73f1970182815f94be94b3bda052f04343156f8
Ariel Behar <ariel.behar@hotmail.com>
2024-04-06 09:33:35 +0300

    Restoring the old working configuration for Render.com

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,4 +1,4 @@
 // export const baseUrl = 'http://localhost:3030/api'
 // export const baseUrl = 'https://ivylu.herokuapp.com/api'
-export const baseUrl = 'https://ivylu.arielbehar.com'
-// export const baseUrl = 'https://ivylu.onrender.com/api'
+// export const baseUrl = 'https://ivylu.arielbehar.com/api'
+export const baseUrl = 'https://ivylu.onrender.com/api'
